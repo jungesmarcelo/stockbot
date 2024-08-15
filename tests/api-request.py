211f3be65de8689fd918fc1a7c13bfc04d119ac9
marcelo.junges <marcelo.junges@byne.com.br>
2024-08-15 00:46:01 -0300

    Add json responde example

--- tests/api-request.py
+++ tests/api-request.py
@@ -3,7 +3,8 @@
 import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=APAS7BR5K3Q0349T'
+#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=VALE&interval=5min&apikey=APAS7BR5K3Q0349T'
+url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=VALE3.SA&apikey=urlAPAS7BR5K3Q0349T'
 r = requests.get(url)
 data = r.json()
 
